f4e6e4e6d51168051855647d44bba0ba5a5308a1
mikhailshahmie <88524530+mikhailshahmie@users.noreply.github.com>
2021-08-27 10:31:48 +0800

    update connect.php

--- connect.php
+++ connect.php
@@ -1,45 +1,84 @@
 ﻿<?php
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+session_start();
 
-    if (!empty($email) || !empty($username) || !empty($password)) {
-        
-            $host = "localhost";
-            $dbusername = "root";
-            $dbpassword = "";
-            $dbname = "dookifos";
-        
-        $conn = new mysqli($host,$dbusername,$dbpassword,$dbname);
-    
-        if(mysqli_connect_error()){
-            die('Connection Failed('. mysqli_connect_errno().')'. mysqli_connect_error());
-        }else{
-        $SELECT = "SELECT email From registration Where email = ? Limit 1";
-        $INSERT = "SELECT Into registration (username, password) values(?, ?)";
-        $stmt = $conn->prepare($SELECT);
-        $stmt->bind_param("s", $email);
-        $stmt->execute();
-        $stmt->bind_result($email);
-        $stmt->store_result();
-        $rnum = $stmt->num_rows;
+// initializing variables
+$username = "";
+$email    = "";
+$errors = array(); 
 
-        if ($rnum==0) {
-            $stmt->close();
+// connect to the database
+$db = mysqli_connect('localhost', 'web2021', 'web2021', 'testingproject');
 
-            $stmt = $conn->prepare($INSERT);
-            $stmt->bind_param("sss", $username, $email, $password); 
-            $stmt->execute();
-            echo "Registration successfully!";
-        }else{
-            echo "Someone already registered using this email";
-        }
-            $stmt->close();
-            $conn->close();
-        }
-    }else{
-        echo "All field are required";
-        die();
+// REGISTER USER
+if (isset($_POST['signup-btn'])) {
+  // receive all input values from the form
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $email = mysqli_real_escape_string($db, $_POST['email']);
+  $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+  $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+
+  // form validation: ensure that the form is correctly filled ...
+  // by adding (array_push()) corresponding error unto $errors array
+  if (empty($username)) { array_push($errors, "Username is required"); }
+  if (empty($email)) { array_push($errors, "Email is required"); }
+  if (empty($password_1)) { array_push($errors, "Password is required"); }
+  if ($password_1 != $password_2) {
+	array_push($errors, "The two passwords do not match");
+  }
+
+  // first check the database to make sure 
+  // a user does not already exist with the same username and/or email
+  $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+  
+  if ($user) { // if user exists
+    if ($user['username'] === $username) {
+      array_push($errors, "Username already exists");
     }
 
+    if ($user['email'] === $email) {
+      array_push($errors, "email already exists");
+    }
+  }
+
+  // Finally, register user if there are no errors in the form
+  if (count($errors) == 0) {
+  	$password = $password_1;//encrypt the password before saving in the database
+
+  	$query = "INSERT INTO customer (username, email, password) 
+  			  VALUES('$username', '$email', '$password')";
+  	mysqli_query($db, $query);
+  	
+  	header('location: menu.php');
+  }
+}
+
+// ... 
+// LOGIN USER
+if (isset($_POST['login-btn'])) {
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $password = mysqli_real_escape_string($db, $_POST['password']);
+
+  if (empty($username)) {
+  	array_push($errors, "Username is required");
+  }
+  if (empty($password)) {
+  	array_push($errors, "Password is required");
+  }
+
+  if (count($errors) == 0) {
+  	$password = $password;
+  	$query = "SELECT * FROM customer WHERE username='$username' AND password='$password'";
+  	$results = mysqli_query($db, $query);
+  	if (mysqli_num_rows($results) == 1) {
+
+  	  header('location: menu.php');
+  	}else {
+  		array_push($errors, "Wrong username/password combination");
+  	}
+  }
+}
+
 ?>
+
