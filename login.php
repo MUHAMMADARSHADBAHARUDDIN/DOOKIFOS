adc71a6b6cbafd56dd4f47335da6092368ecc4c4
mikhailshahmie <88524530+mikhailshahmie@users.noreply.github.com>
2021-08-27 10:28:32 +0800

    change login file html to php

--- login.php
+++ login.php
@@ -1,7 +1,8 @@
-﻿<!DOCTYPE html>
+<?php include('server.php') ?>
+<!DOCTYPE html>
 <html>
 <head>
-    <title>Login page</title>
+    <title>LogIn page</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -9,22 +10,22 @@
 
 </head>
 <body>
-    <form action ="connect.php" method="post">
-        <h1>Login</h1>
-       
+    <form method="post" action="login.php">
+       <?php include('errors.php'); ?>
+        <h1>LogIn</h1>
+
+        
         <hr />
         <label>Username</label>
         <input type="text" placeholder="Username" id="username" name="username" />
         <br />
-        <label>Email</label>
-        <input type="text" placeholder="Email" id="email" name="email" />
-        <br />
         <label>Password</label>
         <input type="text" placeholder="Password" id="password" name="password" />
         <br />
+        
 
-        <button class="btn btn-primary active">Login</button>
-        <p>Did not have an account? <a href="new.html">Sign Up</a></p>
+        <button type="submit" name = "login-btn" class="btn btn-primary active">Sign Up</button>
+        <p>Did not have an account? <a href="register.php">Sign Up</a></p>
     </form>
 </body>
 </html>
