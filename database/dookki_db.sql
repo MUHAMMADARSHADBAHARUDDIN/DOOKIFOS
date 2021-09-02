c3e108ab4736b508b97c599c178398fee162e00c
Muhammad Afiq Haikal bin Mohd Firdus <79618593+Aeroblaster@users.noreply.github.com>
2021-09-02 15:20:15 +0800

    update database file

--- database/dookki_db.sql
+++ database/dookki_db.sql
@@ -1,7 +1,32 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 06, 2021 at 02:22 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.1.26
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `maher`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `addcart`
+--
 
 CREATE TABLE `addcart` (
   `id` bigint(12) NOT NULL,
@@ -12,6 +37,10 @@ CREATE TABLE `addcart` (
   `total` bigint(12) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `addcart`
+--
+
 INSERT INTO `addcart` (`id`, `p_id`, `u_id`, `price`, `qty`, `total`) VALUES
 (2, 1, 'abcd', 100, 4, 400),
 (3, 8, 'abcd', 30, 1, 30),
@@ -19,14 +48,30 @@ INSERT INTO `addcart` (`id`, `p_id`, `u_id`, `price`, `qty`, `total`) VALUES
 (5, 6, 'abcd', 30, 3, 90),
 (6, 11, 'admin', 30, 2, 60);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
 CREATE TABLE `admin` (
   `adminid` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `admin`
+--
+
 INSERT INTO `admin` (`adminid`, `password`) VALUES
 ('admin', 'admin'),
 ('nilkanth', 'nilkanth');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `checkout`
+--
 
 CREATE TABLE `checkout` (
   `id` bigint(12) NOT NULL,
@@ -37,6 +82,10 @@ CREATE TABLE `checkout` (
   `email` varchar(150) NOT NULL,
   `location` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `checkout`
+--
 
 INSERT INTO `checkout` (`id`, `p_id`, `u_id`, `name`, `mobile`, `email`, `location`) VALUES
 (1, 1, 'abcd', '', 0, '', ''),
@@ -63,11 +112,21 @@ INSERT INTO `checkout` (`id`, `p_id`, `u_id`, `name`, `mobile`, `email`, `locati
 (22, 8, 'abcd', 'nilkanth', 9987656789, 'abc@y.co', 'asjd'),
 (23, 11, 'admin', 'hello', 8978678909, 'h@yyy.com', 'jas skkslks kslda');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `food_cat`
+--
+
 CREATE TABLE `food_cat` (
   `id` bigint(12) NOT NULL,
   `catnm` varchar(100) NOT NULL,
   `sub_cat` varchar(120) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `food_cat`
+--
 
 INSERT INTO `food_cat` (`id`, `catnm`, `sub_cat`) VALUES
 (1, 'kathiyawadi', 'Maher Special'),
@@ -106,6 +165,12 @@ INSERT INTO `food_cat` (`id`, `catnm`, `sub_cat`) VALUES
 (55, 'tava', 'Vagharelo Rotlo Dry'),
 (56, 'tava', 'Vagharelo Rotlo Dahi');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `food_parcel`
+--
+
 CREATE TABLE `food_parcel` (
   `id` bigint(12) NOT NULL,
   `name` varchar(100) NOT NULL,
@@ -115,10 +180,20 @@ CREATE TABLE `food_parcel` (
   `food_id` bigint(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gallery`
+--
+
 CREATE TABLE `gallery` (
   `id` bigint(12) NOT NULL,
   `image` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `gallery`
+--
 
 INSERT INTO `gallery` (`id`, `image`) VALUES
 (3, 'img/5c7515fc03eded3e20e78a6f_1551177212604.jpg'),
@@ -130,6 +205,12 @@ INSERT INTO `gallery` (`id`, `image`) VALUES
 (9, 'img/chines.jpg'),
 (10, 'img/abc.jpg');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu`
+--
+
 CREATE TABLE `menu` (
   `id` bigint(12) NOT NULL,
   `category` varchar(100) NOT NULL,
@@ -139,6 +220,10 @@ CREATE TABLE `menu` (
   `price` bigint(6) NOT NULL,
   `image` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `menu`
+--
 
 INSERT INTO `menu` (`id`, `category`, `sub_cat`, `title`, `description`, `price`, `image`) VALUES
 (4, 'kathiyawadi', 'Rajwadi Bateta', 'rajwadi bateta nu shak', 'bateta cut and fry in oil make a shak', 40, 'mimg/e7e0aa16c95cca7deae0e222e8794bb5.jpg'),
@@ -173,12 +258,22 @@ INSERT INTO `menu` (`id`, `category`, `sub_cat`, `title`, `description`, `price`
 (33, 'rise', 'Rajwadi Khichadi', 'rajwadi khichadi', 'masala add in khichadi', 25, 'mimg/rajwadi-khichdi-recipe-main-photo.jpg'),
 (34, 'rajsthani', 'Dal', 'jaydip', 'nathi', 1, 'mimg/chole bhature.jpg');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registration`
+--
+
 CREATE TABLE `registration` (
   `id` bigint(12) NOT NULL,
   `userid` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `email` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `registration`
+--
 
 INSERT INTO `registration` (`id`, `userid`, `password`, `email`) VALUES
 (1, 'abcd', 'xyz', 'abc@yahoo.com'),
@@ -187,6 +282,12 @@ INSERT INTO `registration` (`id`, `userid`, `password`, `email`) VALUES
 (4, 'abcd', 'ppp', 'abc@yahoo.com'),
 (5, 'abcd', 'asdf', 'abc@yahoo.com');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `review`
+--
+
 CREATE TABLE `review` (
   `id` bigint(12) NOT NULL,
   `name` varchar(100) NOT NULL,
@@ -194,49 +295,113 @@ CREATE TABLE `review` (
   `description` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `review`
+--
+
 INSERT INTO `review` (`id`, `name`, `review`, `description`) VALUES
 (5, 'Milan vagh', 'Exellent', 'best hotel oh gujarat'),
 (6, 'naimish gadhiya', 'Exellent', 'abcdf');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `addcart`
+--
 ALTER TABLE `addcart`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `admin`
+--
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`adminid`);
 
+--
+-- Indexes for table `checkout`
+--
 ALTER TABLE `checkout`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `food_cat`
+--
 ALTER TABLE `food_cat`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `gallery`
+--
 ALTER TABLE `gallery`
   ADD PRIMARY KEY (`id`);
-R TABLE `menu`
+
+--
+-- Indexes for table `menu`
+--
+ALTER TABLE `menu`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `registration`
+--
 ALTER TABLE `registration`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `review`
+--
 ALTER TABLE `review`
   ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `addcart`
+--
 ALTER TABLE `addcart`
   MODIFY `id` bigint(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-TABLE `checkout`
+
+--
+-- AUTO_INCREMENT for table `checkout`
+--
+ALTER TABLE `checkout`
   MODIFY `id` bigint(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
+--
+-- AUTO_INCREMENT for table `food_cat`
+--
 ALTER TABLE `food_cat`
   MODIFY `id` bigint(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
+--
+-- AUTO_INCREMENT for table `gallery`
+--
 ALTER TABLE `gallery`
   MODIFY `id` bigint(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
- TABLE `menu`
+
+--
+-- AUTO_INCREMENT for table `menu`
+--
+ALTER TABLE `menu`
   MODIFY `id` bigint(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
+--
+-- AUTO_INCREMENT for table `registration`
+--
 ALTER TABLE `registration`
   MODIFY `id` bigint(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
+--
+-- AUTO_INCREMENT for table `review`
+--
 ALTER TABLE `review`
   MODIFY `id` bigint(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
