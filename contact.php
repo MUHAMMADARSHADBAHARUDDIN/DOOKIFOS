e03c94295add887b9a74bb4b77612a0fc6ba66f8
Muhammad Afiq Haikal bin Mohd Firdus <79618593+Aeroblaster@users.noreply.github.com>
2021-09-05 21:14:30 +0800

    create contact.php

---
+++ contact.php
@@ -0,0 +1,58 @@
+<?php include "header.php"; ?>	
+	<!-- Start All Pages -->
+		
+		<img src="images/cn.png" width="100%">
+	<!-- End All Pages -->
+	
+	<!-- Start Contact -->
+	<div>
+	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1992.23014045411!2d101.71205992096874!3d2.96970039780989!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cdcb5918f74139%3A0xb30207d26754e96!2sDookki%20Malaysia%20%40%20IOI%20City%20Mall!5e0!3m2!1sen!2smy!4v1630569215535!5m2!1sen!2smy" width="2500" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+
+	</div>
+	<div class="contact-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="heading-title text-center">
+						<h2>Contact</h2>
+						<p>Contact Information are Listed here Thanks for Visit Our Site</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12">
+					<form id="contactForm">
+						<div class="row">
+							<div class="col-md-12">
+								<div class="form-group">
+									<h1 style="font-family: cambria; color: red; text-align: center;">Dokki Malaysia</h1>
+									<div class="help-block with-errors"></div>
+								</div>                                 
+							</div>
+							<div class="col-md-12">
+								<div class="form-group">
+									<center><span style="color: tomato; font-size: 1.3em; text-align: center; "><b>Email Us :</b> dookkimalaysia@gmail.com</span></center>
+									<div class="help-block with-errors"></div>
+								</div> 
+							</div>
+							<div class="col-md-12">
+								<div class="form-group">
+									<center><span style="color: tomato; font-size: 1.3em; text-align: center; "><b>Mobile  :</b> +603-8685-4439</span></center>
+									<div class="help-block with-errors"></div>
+								</div> 
+							</div>
+							<div class="col-md-12">
+								<div class="form-group"> 
+									<center><span style="color: tomato; font-size: 1.3em; text-align: center; "><b>Location  :</b> L1-12B, IOI City Mall, Lebuh IRC, IOI Resort, 62502 Putrajaya</span></center>
+									<div class="help-block with-errors"></div>
+								</div>
+								
+							</div>
+						</div>            
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End Contact -->
+<?php include "footer.php"; ?>
