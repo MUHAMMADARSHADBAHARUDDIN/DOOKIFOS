30e51e09b181322a5eef406f5ad510692deda1be
mikhailshahmie <88524530+mikhailshahmie@users.noreply.github.com>
2021-09-25 22:45:35 +0800

    Design Changes

--- registration.php
+++ registration.php
@@ -8,7 +8,7 @@
 		<div class="container text-center">
 			<div class="row">
 				<div class="col-lg-12">
-					<h1> REGISTRATION US </h1>
+					<h1> REGISTRATION </h1>
 				</div>
 			</div>
 		</div>
