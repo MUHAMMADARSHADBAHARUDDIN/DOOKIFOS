cc72835289ee79739ab20e322be07d18d58c444d
Muhammad Afiq Haikal bin Mohd Firdus <79618593+Aeroblaster@users.noreply.github.com>
2021-10-04 10:58:45 +0800

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,86 +1,86 @@
+<?php session_start();
+    $uid = $_SESSION['uid'];
+    include "header.php"; ?>
+<!DOCTYPE html>
+<html>
+<style>
+.button {
+  top:10%;
+  left:55%;
+  background-color: blue;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  width:  200px;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  display:inline-block;
+ 
+}
 
-<?php include "header.php"; ?> 
-<body>
-	
-	<!-- Start All Pages -->
-	<center> <img src="feedback.jpg" style="background-color: red"> </center>
-	<!-- End All Pages -->
-	
-	<!-- Start Contact -->
-	
-	<div class="contact-box">
-		<div class="container">
+</style>
+<div class="all-page-title page-breadcrumb">
+		<div class="container text-center">
 			<div class="row">
 				<div class="col-lg-12">
-					<div class="heading-title text-center">
-						
-						<p>Our Main Goal is Client Satisfaction</p>
-					</div>
+					<h1>Update User Profile</h1>
 				</div>
 			</div>
-
-			<div class="row">
-				<div class="col-lg-12">
-					<form  action="" method="post">
-						<div class="row">
-							<div class="col-md-12">
-								<div class="form-group">
-									<input type="text" class="form-control" id="name" name="name" placeholder="Your Name" required data-error="Please enter your name">
-									<div class="help-block with-errors"></div>
-								</div>                                 
-							</div>
-							<!-- <div class="col-md-12">
-								<div class="form-group">
-									<input type="text" placeholder="Your Email" id="email" class="form-control" name="name" required data-error="Please enter your email">
-									<div class="help-block with-errors"></div>
-								</div> 
-							</div> -->
-							<div class="col-md-12">
-								<div class="form-group">
-									<select class="custom-select d-block form-control" id="guest" name="rev" required data-error="Please Select Person">
-									  <option value="Exellent">Exellent</option>
-									  <option value="Good">Good</option>
-									  <option value="Poor">Poor</option>
-									  <option value="Very Poor">Very Poor</option>
-									  
-									</select>
-									<div class="help-block with-errors"></div>
-								</div> 
-							</div>
-							<div class="col-md-12">
-								<div class="form-group"> 
-									<textarea class="form-control" id="message" placeholder="Your Message" rows="4" name="desc" data-error="Write your message" required></textarea>
-									<div class="help-block with-errors"></div>
-								</div>
-								<div class="submit-button text-center">
-									<button name="sb" class="btn btn-common" id="submit" type="submit">Send Message</button>
-									<div id="msgSubmit" class="h3 text-center hidden"></div> 
-									<div class="clearfix"></div> 
-								</div>
-							</div>
-						</div>            
-					</form>
-					<?php
-					if(isset($_POST['sb']))
-					{
-						$nm = $_POST['name'];
-						$rev = $_POST['rev'];
-						$des = $_POST['desc'];
-						include "connect.php";
-						mysqli_query($con,"insert into review(name, review, description) values('$nm','$rev','$des')");
-						echo "<h2 style='color:tomato; text-align:center;'>Data Addedd SuccessFully</h2>";
-					}	
-
-					?>
-
-
-
-
-
-				</div>
-			</div>
-		
 		</div>
 	</div>
-	<!-- End Contact -->
+<body>
+
+<center>
+<!--<p><a href="admin_login.php" class="button">Admin Login</a></p>-->
+<p>
+
+
+</p>
+	<div class="row">
+		<div class="col-md-8 offset-2">
+			<form action="">
+				<?php
+				$conn = mysqli_connect('localhost','root','','dookki_db');
+				$currentUser = $_SESSION['uid'];
+				$sql = "SELECT * FROM registration WHERE userid  = '$currentUser'";
+
+					$gotResults = mysqli_query($conn,$sql);
+
+					if($gotResults){
+						if(mysqli_num_rows($gotResults)>0){
+							while($row = mysqli_fetch_array($gotResults)){
+								//print_r($row['userid']);
+								?>
+									<div class="col-md-6">
+										<div class="form-group">
+											Username : <input type="text" name="updateusername" class="form-control" value="<?php echo $row['userid']; ?>">
+										</div>
+										<div class="form-group">
+											Email : <input type="email" name="useremai" class="form-control" value="<?php echo $row['email']; ?>">
+										</div>
+										<div class="form-group">
+											Password : <input name="userpassword" class="form-control" value="<?php echo $row['password']; ?>">
+										</div>
+										<div class="form-group">
+											<input type="file" name="userimage" class="form-control">
+										</div>
+					
+										<div class="form-group">
+											<input type="submit" name="update" class="btn btn-info" value="Update">
+										</div>
+									</div>
+								<?php
+							}
+						}
+					}
+				?>
+			</form>
+		</div>
+	</div>
+</body>
+</html>
 <?php include "footer.php"; ?>
