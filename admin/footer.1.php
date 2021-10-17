6ef477aeb7eec48ea7e09cda5976613e625ea980
Muhammad Afiq Haikal bin Mohd Firdus <79618593+Aeroblaster@users.noreply.github.com>
2021-10-17 23:43:01 +0800

    major delete

--- admin/footer.1.php
+++
@@ -1,2 +0,0 @@
-<br><br>
-<div class="footer"> Design & Developed By: Group 4</div>
--- processFood.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "foodFunction.php";
-//print_r($_POST)
-	if(isSet($_POST['updateFoodButton']))
-	{
-	updateFacilityInfo();
-	header( "refresh:0; url=view_food2.php");
-	}
-?>
