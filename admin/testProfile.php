3854b81c99a9265d75f5abfd6b59a0ef1076d9d1
Muhammad Afiq Haikal bin Mohd Firdus <79618593+Aeroblaster@users.noreply.github.com>
2021-10-18 10:36:07 +0800

    Update testProfile.php

--- admin/testProfile.php
+++ admin/testProfile.php
@@ -74,7 +74,7 @@ function myFunction() {
 										<input type="hidden" name="uID" class="form-control" value="'.$row['id'].'">
 										</div>
 										<div class="form-group">
-											Username : <input type="text" name="updateusername" class="form-control" value="'.$row['adminid'].'">
+											Username : <input type="text" name="updateusername" class="form-control" id="myInput" value="'.$row['adminid'].'">
 										</div>
 										<div class="form-group">
 											Email : <input type="email" name="useremail" class="form-control" value="'.$row['email'].'">
