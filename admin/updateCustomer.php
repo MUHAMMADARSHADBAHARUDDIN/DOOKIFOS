6c8cb16bd5de6694b29aa4f7436af2eff2a06969
Muhammad Afiq Haikal bin Mohd Firdus <79618593+Aeroblaster@users.noreply.github.com>
2021-11-18 23:18:57 +0800

    Update updateCustomer.php

--- admin/updateCustomer.php
+++ admin/updateCustomer.php
@@ -97,7 +97,7 @@ function myFunction() {
 				 <?php
 				 if(isset($_POST['updateCustomer']))
 				 {
-					 $con = mysqli_connect("localhost","root","","dookki_db");
+					$con = mysqli_connect("localhost","dookkifo_server","DookkiMyG4","dookkifo_dookki_db");
 					 if(!$con)
 						 {
 						 echo mysqli_error();
