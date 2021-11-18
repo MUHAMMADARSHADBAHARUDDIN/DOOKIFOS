b4358b076b5a3660f3e0dd3bc2da87595b975c7d
mikhailshahmie <88524530+mikhailshahmie@users.noreply.github.com>
2021-11-18 23:49:45 +0800

    testProfile

--- testProfile.php
+++ testProfile.php
@@ -104,7 +104,7 @@ function myFunction() {
 				 <?php
 				 if(isset($_POST['updateProfile']))
 				 {
-					 $con = mysqli_connect("localhost","root","","dookki_db");
+					$con = mysqli_connect("localhost","dookkifo_server","DookkiMyG4","dookkifo_dookki_db");
 					 if(!$con)
 						 {
 						 echo mysqli_error();
